0a502af6d373e04522d74ddf3b545da2fb6cc9c3
Chris Tipney <chris@tipney.net>
2023-03-13 11:34:44 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@ int main()
 {
 
     // TODO - write name to screen
-    for (int n=10; n>=0; n--);
+    for (int n = 10; n >= 0; n--)
+        ;
     {
         printf("Hello World\n");
     }
 
     int aa = 2;
     int bb = 3;
+    int cc = 5;
+    int dd = 1;
 
-    int y = add(aa,bb);
-    
+    int y = add(aa, bb);
+    int z = sub(cc, dd);
     return 0;
 }
